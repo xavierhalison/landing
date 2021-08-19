c7d8c1e6505a6b0f1c2b6be18bdda72662130894
xavierhalison <halisonpx@gmail.com>
2021-08-19 10:21:16 -0300

    more section

--- src/components/sections/dropdownTexts/index.js
+++ src/components/sections/dropdownTexts/index.js
@@ -17,37 +17,40 @@ function CardSection() {
 
   return (
     <Container id="dropdowns">
-      <Title gradient>Achei que faria sentido ter título aqui também</Title>
+      <Title gradient>More about me</Title>
       <DropdownGrid>
         <DropdownCard
-          title="Título do card"
+          title="Software projects"
           expanded={expandedCard[0]}
           toggle={() => toggle(0)}
         >
-          In publishing and graphic design, Lorem ipsum is a placeholder text
-          commonly used to demonstrate the visual form of a document or a
-          typeface without relying on meaningful content. Lorem ipsum may be
-          used as a placeholder before final copy is available.
+          I've built this page as a model of a simple and generic Landing Page
+          that can work well with several types of business. To make this I've
+          use basically React since its a very simple Single Page Application.
+          My goal with this is to reuse its structure when someone ask me to
+          create a landing page, but I will host the whole code in Github to
+          everyone who's interested.
         </DropdownCard>
         <DropdownCard
-          title="Título do card"
+          title="Hobbies and Personal Life"
           expanded={expandedCard[1]}
           toggle={() => toggle(1)}
         >
-          In publishing and graphic design, Lorem ipsum is a placeholder text
-          commonly used to demonstrate the visual form of a document or a
-          typeface without relying on meaningful content. Lorem ipsum may be
-          used as a placeholder before final copy is available.
+          My life isn't just code, I'm very far from this actually. I really
+          enjoy music, especially metal music, and it's part of my lifestyle, I
+          love to be part of the musical scene playing in bands and going to
+          events. I also love classic cars and own a 1975 VW Beetle which I've
+          been working in it's restauration in the last months.
         </DropdownCard>
         <DropdownCard
-          title="Título do card"
+          title="Future Projects"
           expanded={expandedCard[2]}
           toggle={() => toggle(2)}
         >
-          In publishing and graphic design, Lorem ipsum is a placeholder text
-          commonly used to demonstrate the visual form of a document or a
-          typeface without relying on meaningful content. Lorem ipsum may be
-          used as a placeholder before final copy is available.
+          I want to incorporate a CMS to this space so I'll can write about tech
+          related things. I also want to expand my participation in the musical
+          scene using my development skills to create applications in order to
+          support independent artist in my area.
         </DropdownCard>
       </DropdownGrid>
       <MainCTA>Teste</MainCTA>
